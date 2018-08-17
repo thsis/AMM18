58f997e41122ffdbbc352fcfb5cfd978b17d7fb5
Thomas Siskos <thomas.siskos91@gmail.com>
2018-08-17 08:48:13 +0200

    Drop cola data and summarise stores.

--- data/soda-data-preparation.py
+++ data/soda-data-preparation.py
@@ -17,7 +17,6 @@ from glob import glob
 week_path = os.path.join("data", "IRI week translation_2008_2017.csv")
 weather_path = os.path.join("data", "Weather_Boston_*")
 sugar_path = os.path.join("data", "week_sugar.csv")
-cola_path = os.path.join("data", "cola_amm_boston.csv")
 soda_path = os.path.join("data", "GingerAleLemonDew_Chain139.csv")
 outpath = os.path.join("data", "soda.csv")
 
@@ -55,29 +54,12 @@ weather = weather.groupby("week")["temperature_high",
                                   "temperature_avg",
                                   "temperature_low"].mean()
 
-# Task Nr.2: sugar, cola and soda data.
+# Task Nr.2: sugar and soda data.
 sugar = pd.read_csv(sugar_path)
-cola = pd.read_csv(cola_path)
 soda = pd.read_csv(soda_path)
 
 
 # Task Nr 3: create compount dataset.
-# Harmonise columns in cola.
-def flavor_mapper(x):
-    if "DIET" in x:
-        return "DIET"
-    else:
-        return "REGULAR"
-
-
-cola.rename(columns={"feature": "f", "display": "d"}, inplace=True)
-cola.drop(["price_deflated", "total_rev_cola", "total_rev_l4",
-           "total_vol_carbbev", "total_vol_cola", "total_vol_l4"],
-          axis=1, inplace=True)
-cola["FLAVOR/SCENT"] = "COLA"
-cola["CALORIE LEVEL"] = cola.L5.apply(flavor_mapper)
-
-
 # Groom rows and columns of soda.
 def l5_mapper(x):
     label = ""
@@ -92,20 +74,19 @@ soda["L5"] = soda.apply(l5_mapper, axis=1)
 soda.drop("PRODUCT TYPE", axis=1, inplace=True)
 
 # Aggregate out the upc column
-agg_vars = ["MARKET", "CHAIN", "store_type", "iri_key", "week", "year", "L4",
+agg_vars = ["MARKET", "CHAIN", "store_type", "week", "year", "L4",
             "L5", "VOL_EQ", "PACKAGE", "FLAVOR/SCENT", "CALORIE LEVEL"]
-agg = soda.groupby(agg_vars).aggregate({"units": np.sum,
-                                        "dollars": np.sum,
-                                        "price": np.mean,
-                                        "f": np.unique,
-                                        "d": np.unique})
-data = soda.drop(["upc", "units", "dollars", "price", "f", "d"], axis=1)
-data = data.merge(agg, left_on=agg_vars, right_index=True)
-# NOTE: we LOSE 45 rows, since different upc's are on display/feature or not.
-data = data.loc[data.f.astype(str).apply(lambda x: x in soda.f.unique())]
-data = data.loc[data.d.astype(str).apply(lambda x: x in ['0', '1', '2'])]
+# Get dummies.
+soda = pd.get_dummies(soda, columns=["d", "f"], drop_first=True, dtype=bool)
+soda["feature_all"] = (~soda["f_NONE"]).astype(int)
+soda["display_all"] = soda["d_1"] | soda["d_2"]
 
-data = pd.concat([data, cola], sort=False)
+# Aggregate - Drop upc's + stores.
+data = soda.groupby(agg_vars).aggregate({"units": np.sum,
+                                         "dollars": np.sum,
+                                         "price": np.mean,
+                                         "feature_all": np.mean,
+                                         "display_all": np.mean})
 
 # Task 4: Merge weather and sugar data.
 data = data.merge(weather, on=["week"], how='left')
